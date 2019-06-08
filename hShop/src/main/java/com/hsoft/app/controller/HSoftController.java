6c8532202fd765f33318167cd1e35b5531c5dd62
tarung1201 <tarung1201@gmail.com>
2019-06-08 18:04:24 +0530

    Update HSoftController.java

--- hShop/src/main/java/com/hsoft/app/controller/HSoftController.java
+++ hShop/src/main/java/com/hsoft/app/controller/HSoftController.java
@@ -35,7 +35,7 @@ public class HSoftController {
 	}
 	
 	@GetMapping("/getRole")
-	public String getRole(@RequestBody String roleName) {
+	public String getRole() {
 //		userRepo.save(user);
 		return "Created User";
 	}
