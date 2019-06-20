bdd4b79166666540f9833e0f7f8a2913625faa9d
Himanshu Garg <garghimanshu252@gmail.com>
2019-06-20 17:33:29 +0530

    Prefix suffix done

--- hShop/src/main/java/com/hsoft/app/repository/PatientRepository.java
+++ hShop/src/main/java/com/hsoft/app/repository/PatientRepository.java
@@ -15,7 +15,7 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 	@Query(value = "SELECT * FROM Patient p WHERE p.patient_number LIKE CONCAT('%',:searchTerm,'%')", nativeQuery = true)
 	List<Patient> searchWithJPQLQuery(@Param("searchTerm") String searchTerm);
 	
-	@Query(value = " select currval('Patient_id_seq')", nativeQuery = true)
+	@Query(value = " select last_value FROM patient_seq", nativeQuery = true)
 	long currentValue(); 
     
 	
