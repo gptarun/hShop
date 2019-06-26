ed183dc8ebef702c2bdbdb01cc6163b934d1eafe
tarung1201 <tarung1201@gmail.com>
2019-06-26 21:21:20 +0530

    Update import.sql

--- hShop/src/main/resources/import.sql
+++ hShop/src/main/resources/import.sql
@@ -1,15 +1,17 @@
-INSERT INTO public.department(dep_id, department) VALUES (1000,'Medical');
-INSERT INTO public.role(role_id, role_name) VALUES (1000, 'Admin');
+INSERT INTO public.department(dep_id, department) VALUES (1000,'Admin');
+INSERT INTO public.role(role_id, role) VALUES (1000, 'Admin');
 
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (1, 'EMR');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (2, 'Settings');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (3, 'Shift Management');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (4, 'Billing & Payments');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (5, 'Nurses Desk');
+INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (6, 'Pharmacy');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (7, 'Clerking');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (8, 'Laboratory');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (9, 'Radiology');
 INSERT INTO public.parent_module(parent_module_id, module_name) VALUES (10, 'Reports');
+
 
 INSERT INTO public.module(module_id, module_name, parent_module_id) VALUES (1, 'Patient Registration', '1');
 INSERT INTO public.module(module_id, module_name, parent_module_id) VALUES (2, 'Edit Patient Details', '1');
